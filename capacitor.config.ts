96d11f1f482ce0fe7b3258a21f300c7b6bd8e904
Vaclav Fencl <v_fencl@utb.cz>
2024-12-01 10:58:04 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'dad-jokes',
+  webDir: 'www'
+};
+
+export default config;
