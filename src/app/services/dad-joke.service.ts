7fa10d68a7c7e890212803b674fd1a0f3bd2ebef
Vaclav Fencl <v_fencl@utb.cz>
2024-12-01 13:15:05 +0100

    tab 1 - generování náhodného vtipu za pomoci servisky

--- src/app/services/dad-joke.service.ts
+++ src/app/services/dad-joke.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DadJokeService {
+  private apiUrl = 'https://icanhazdadjoke.com/';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  /**
+   * Fetch a random dad joke.
+   * @returns {Observable<any>} 
+   */
+  getRandomJoke(): Observable<any> {
+    return this.http.get(this.apiUrl, {
+      headers: { Accept: 'application/json' },
+    });
+  }
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DadJokeService } from '../services/dad-joke.service';
 
 @Component({
   selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  templateUrl: './tab1.page.html',
+  styleUrls: ['./tab1.page.scss'],
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  joke: string = ''; 
 
-  constructor() {}
+  constructor(private dadJokeService: DadJokeService) {}
 
+  ngOnInit() {
+    this.getNewJoke(); 
+  }
+
+  getNewJoke() {
+
+    this.dadJokeService.getRandomJoke().subscribe({
+      next: (response) => {
+        this.joke = response?.joke || 'No joke found.'; 
+      },
+      error: (error) => {
+        console.error('Error fetching joke:', error);
+        this.joke = 'Oops! Unable to fetch a joke right now.';
+      },
+    });
+  }
 }
